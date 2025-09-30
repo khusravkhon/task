4bc1f7560c19f9948ea9fa9e9931ed9544284cd4
khusrav <kroydjaykven@gmail.com>
2025-09-30 19:36:33 +0500

    main

---
+++ leetcode/30 Days of JavaScript/2621_Sleep.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(millis);
+    }, millis);
+  });
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
+
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
